c86be56b56eb4a6911249d32f702d81994a4f13c
BlakeIsMyWaifu <33188727+BlakeIsMyWaifu@users.noreply.github.com>
2022-01-29 12:51:35 +0000

    good morning vibes init

---
+++ GoodMorningVibes/Framework/ModConfig.cs
@@ -0,0 +1,12 @@
+namespace BlakeIsMyWaifu.Stardew.GoodMorningVibes
+{
+    public class ModConfig
+    {
+        public string[] Messages { get; set; }
+
+        public ModConfig()
+        {
+            this.Messages = new string[] { "Good Morning" };
+        }
+    }
+}
---
+++ GoodMorningVibes/ModEntry.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using Microsoft.Xna.Framework;
+
+namespace BlakeIsMyWaifu.Stardew.GoodMorningVibes
+{
+    public class ModEntry : Mod
+    {
+        private ModConfig _modConfig;
+
+        public override void Entry(IModHelper helper)
+        {
+            this._modConfig = this.Helper.ReadConfig<ModConfig>();
+
+            helper.Events.GameLoop.DayStarted += SendReminder;
+        }
+
+        private void SendReminder(object sender, DayStartedEventArgs e)
+        {
+            string[] messages = _modConfig.Messages;
+            Random random = new Random();
+            int index = random.Next(0, messages.Length);
+            string message = messages.Length != 0 ? messages[index] : "Good Morning";
+
+            HUDMessage hudMessage = new HUDMessage(message, Color.SeaGreen, 8000f);
+            hudMessage.noIcon = true;
+
+            Game1.addHUDMessage(hudMessage);
+        }
+    }
+}
